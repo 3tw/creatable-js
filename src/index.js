8f4c03a42a8a8122a6d73fd3fff97ad0eb8677a8
Teo Winkler <teo.winkler@gmail.com>
2021-07-06 23:10:14 +0200

    fix: create a functioning npm module

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import creatable_append from './append.js'
-import creatable_attribute from './attribute.js'
-import creatable_addClass from './addClass.js'
-import creatable_insertBefore from './insertBefore.js'
-import creatable_insertAfter from './insertAfter.js'
-import creatable_prepend from './prepend.js'
-import creatable_removeClass from './removeClass.js'
-import creatable_select from './select.js'
-import creatable_selectChild from './selectChild.js'
-import creatable_selectNext from './selectNext.js'
-import creatable_selectParent from './selectParent.js'
-import creatable_selectPrev from './selectPrev.js'
-import creatable_text from './text.js'
+import creatable_append from './creatable/append.js'
+import creatable_attribute from './creatable/attribute.js'
+import creatable_addClass from './creatable/addClass.js'
+import creatable_insertBefore from './creatable/insertBefore.js'
+import creatable_insertAfter from './creatable/insertAfter.js'
+import creatable_prepend from './creatable/prepend.js'
+import creatable_removeClass from './creatable/removeClass.js'
+import creatable_select from './creatable/select.js'
+import creatable_selectChild from './creatable/selectChild.js'
+import creatable_selectNext from './creatable/selectNext.js'
+import creatable_selectParent from './creatable/selectParent.js'
+import creatable_selectPrev from './creatable/selectPrev.js'
+import creatable_text from './creatable/text.js'
 
-export function Creatable(selector = null) {
-  this.el = selector
+export default function Creatable(selector = null) {
+  this.el = selector ? document.querySelector(selector) : null
 }
 
 Creatable.prototype = {
--- webpack.mix.cjs
+++
@@ -1,5 +0,0 @@
-const mix = require('laravel-mix')
-
-mix
-  .js(['src/index.js', 'src/demo.js'], 'demo/demo.js')
-  .js('src/index.js', 'dist/create-element.js')
